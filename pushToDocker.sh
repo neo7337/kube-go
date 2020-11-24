eb7a59a1fbac60af24f354e2c5fb6328b764d392
Aditya Kumar <adityak007mr@gmail.com>
2020-11-24 17:58:53 +0530

    docker build testing

--- pushToDocker.sh
+++ pushToDocker.sh
@@ -1,3 +1,23 @@
 #! /bin/sh
 
+set -e
+
+if [[ $# -lt 1 ]]; then
+    echo "Tag ref not present, exiting script execution"
+    exit 1
+fi
+
+echo "Building Image"
+docker build -t neo73/kube-go:$1 .
+
+echo "Pushing Tag :$1"
+docker push neo73/kube-go:$1
+
+echo "Listing Images"
 docker images
+
+echo "Tagging Image as latest"
+#docker tag neo73/kube-go:$1 neo73/kube-go:latest
+
+echo "Push Tag :latest"
+#docker push neo73/kube-go:latest
