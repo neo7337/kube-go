0bcef1aaa9d7ef91781ad721be7018e41e3637e4
Aditya Kumar <adityak007mr@gmail.com>
2020-11-27 23:39:48 +0530

    PR CI Saperated

--- pushToDocker.sh
+++ pushToDocker.sh
@@ -2,22 +2,27 @@
 
 set -e
 
-if [ $# -lt 2 ]; then
-    echo "Correct Input <<USERNAME>> <<TAG REF>> not present, exiting script execution"
+if [ $# -lt 3 ]; then
+    echo "Correct Input <<USERNAME>> <<TAG REF>> <<EVENT_NAME>> not present, exiting script execution"
     exit 1
 fi
 
-echo "Building Image"
-docker build -t $1/kube-go:$2 .
+if [ $3 == 'pull_request' ]; then
+    echo "Building Image"
+    docker build -t $1/kube-go:$2 .
+else
+    echo "Building Image"
+    docker build -t $1/kube-go:$2 .
 
-echo "Pushing Tag :$2"
-docker push $1/kube-go:$2
+    echo "Pushing Tag :$2"
+    docker push $1/kube-go:$2
 
-echo "Listing Images"
-docker images
+    echo "Listing Images"
+    docker images
 
-echo "Tagging Image as latest"
-docker tag $1/kube-go:$2 neo73/kube-go:latest
+    echo "Tagging Image as latest"
+    docker tag $1/kube-go:$2 neo73/kube-go:latest
 
-echo "Push Tag :latest"
-docker push neo73/kube-go:latest
+    echo "Push Tag :latest"
+    docker push neo73/kube-go:latest
+fi
