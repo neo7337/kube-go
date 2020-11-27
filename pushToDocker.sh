b15b537e317082a4441d5fbf6090552da21daebd
Aditya Kumar <adityak007mr@gmail.com>
2020-11-27 16:11:50 +0530

    releasing docker push functionality with versioning

--- pushToDocker.sh
+++ pushToDocker.sh
@@ -8,16 +8,16 @@ if [ $# -lt 2 ]; then
 fi
 
 echo "Building Image"
-docker build -t neo73/kube-go:$2 .
+docker build -t $1/kube-go:$2 .
 
 echo "Pushing Tag :$2"
-#docker push neo73/kube-go:$2
+docker push $1/kube-go:$2
 
 echo "Listing Images"
 docker images
 
 echo "Tagging Image as latest"
-#docker tag neo73/kube-go:$2 neo73/kube-go:latest
+docker tag $1/kube-go:$2 neo73/kube-go:latest
 
 echo "Push Tag :latest"
-#docker push neo73/kube-go:latest
+docker push neo73/kube-go:latest
